f45d8e7320a6625a9511419e9fd5af851eafb06d
Yuli Zhang Hung <yuzhanghung@berkeley.edu>
2026-07-29 18:10:54 -0700

    Migrate artifact storage from MinIO to AWS S3

--- app/storage.py
+++ app/storage.py
@@ -7,9 +7,8 @@ load_dotenv()
 
 s3_client = boto3.client(
     "s3",
-    endpoint_url=os.getenv("S3_ENDPOINT"),
-    aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
-    aws_secret_access_key=os.getenv("S3_SECRET_KEY"),
-    region_name="us-east-1"
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION")
 )
 
