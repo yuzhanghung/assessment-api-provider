77710b0db46e7d303e9053a7ef3c2878a47e64cd
Yuli Zhang Hung <yuzhanghung@berkeley.edu>
2026-07-29 19:24:31 -0700

    fix S3 presigned URL regional endpoint

--- app/storage.py
+++ app/storage.py
@@ -1,6 +1,7 @@
 import os
 
 import boto3
+from botocore.config import Config
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +10,12 @@ s3_client = boto3.client(
     "s3",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION")
+    region_name=os.getenv("AWS_REGION"),
+    config=Config(
+        signature_version="s3v4",
+        s3={
+            "addressing_style": "virtual"
+        }
+    )
 )
 
